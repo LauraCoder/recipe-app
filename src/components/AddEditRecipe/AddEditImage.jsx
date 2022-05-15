bda3d817a42ba2fb2c57e660f7af2e2907e24026
LauraCoder <laura.pelkonen@student.laurea.fi>
2022-05-15 19:42:26 +0300

    Deleting image in EditRecipe -form

--- src/components/AddEditRecipe/AddEditImage.jsx
+++ src/components/AddEditRecipe/AddEditImage.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 //import { Permissions } from 'expo'
 import * as ImagePicker from 'expo-image-picker'
-import { View, StyleSheet, Image, } from 'react-native'
+import { View, StyleSheet, Image, Alert } from 'react-native'
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons'
+import FontIcon from 'react-native-vector-icons/FontAwesome5'
 import Button from '../Button'
 import { TouchableOpacity } from 'react-native'
 import { Overlay } from 'react-native-elements'
+import theme from '../../theme'
 
 
 const styles = StyleSheet.create({
@@ -21,13 +23,21 @@ const styles = StyleSheet.create({
   cameraIcon: {
     position: 'absolute',
     alignSelf: 'center'
-  }
+  },
 })
 
-const AddImageOverlay = ({ toggleOverlay, visible, pickImage, takePicture }) => (
-  <Overlay isVisible={visible} onBackdropPress={toggleOverlay} overlayStyle={{ width: '90%', paddingHorizontal: 5, paddingBottom: 20, display: 'flex', flexDirection: 'row' }}>
-    <Button onPress={pickImage} style={{ flex: 0.5 }}>From gallery</Button>
-    <Button onPress={takePicture} style={{ flex: 0.5 }}>Take a photo</Button>
+const AddImageOverlay = ({ toggleOverlay, visible, pickImage, takePicture, deleteImage }) => (
+  <Overlay isVisible={visible} onBackdropPress={toggleOverlay} overlayStyle={{ width: '90%', paddingHorizontal: 5, paddingBottom: 20, display: 'flex', flexDirection: 'column' }}>
+    <View style={{ isplay: 'flex', flexDirection: 'row' }}>
+      <Button onPress={pickImage} style={{ flex: 0.5 }}>From gallery</Button>
+      <Button onPress={takePicture} style={{ flex: 0.5 }}>Take a photo</Button>
+    </View>
+    <View style={{ display: 'flex', flexDirection: 'row', alignSelf: 'center' }}>
+      <Button secondary onPress={deleteImage} style={{ flex: 0.5 }}>
+        <FontIcon name='trash' color={theme.colors.primary} size={15} />
+        Delete image
+      </Button>
+    </View>
   </Overlay>
 )
 
@@ -39,6 +49,25 @@ const AddImage = ({ values }) => {
   const imageExist = image || values.image
 
   const toggleOverlay = () => setVisible(!visible)
+
+  const deleteImage = () => {
+    values.image = null
+    setVisible(!visible)
+  }
+
+  const deleteAlert = () => {
+    Alert.alert(
+      'Are you sure you want to delete this image?',
+      '',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel'
+        },
+        { text: 'Delete', onPress: () => deleteImage() }
+      ]
+    )
+  }
 
   const pickImage = async () => {
     await ImagePicker.requestMediaLibraryPermissionsAsync()
@@ -53,8 +82,6 @@ const AddImage = ({ values }) => {
       aspect: [4, 3],
       quality: 1,
     })
-
-    console.log('result', result)
 
     if (!result.cancelled) {
       setImage(result.uri)
@@ -91,6 +118,9 @@ const AddImage = ({ values }) => {
           </View>
         )
       }
+        <TouchableOpacity onPress={() => deleteImage()}>
+          <FontIcon name='trash' size={25} style={styles.deleteIcon} />
+        </TouchableOpacity>
       */
   return (
     <TouchableOpacity onPress={toggleOverlay} activeOpacity={.8}>
@@ -106,6 +136,7 @@ const AddImage = ({ values }) => {
         setVisible={setVisible}
         pickImage={pickImage}
         takePicture={takePicture}
+        deleteImage={deleteAlert}
       />
     </TouchableOpacity>
   )
