daddfa148efa2dc4ddbb6952a80fa56da1733047
LauraCoder <laura.pelkonen@student.laurea.fi>
2022-07-02 19:35:52 +0300

    Finished visual styling for search bar

--- src/components/AppBarTop/SearchOverlay.jsx
+++ src/components/AppBarTop/SearchOverlay.jsx
@@ -1,32 +1,57 @@
 import { SearchBar, Overlay } from 'react-native-elements'
-import { TouchableOpacity } from 'react-native'
-import Text from '../Text'
+import { StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/AntDesign'
+import theme from '../../theme'
+import Button from '../Button'
+
+const styles = StyleSheet.create({
+  overlay: {
+    width: 350,
+    padding: 5,
+    flexDirection: 'row'
+  },
+  searchBar: {
+    backgroundColor: '#fff',
+    borderBottomColor: 'transparent',
+    borderTopColor: 'transparent',
+    flex: 0.9,
+    paddingLeft: 0
+  },
+  searchButton: {
+    flexDirection: 'row',
+    flex: 0.1,
+    backgroundColor: theme.colors.backgroundSecondary,
+    borderRadius: 10,
+    paddingRight: 14,
+    paddingLeft: 14,
+    marginTop: 0,
+  },
+})
 
 const SearchOverlay = ({ toggleOverlay, visible, title, updateSearch, onSubmitSearch }) => (
   <Overlay
     isVisible={visible}
     onBackdropPress={toggleOverlay}
-    overlayStyle={{ width: 300, padding: 5 }}
+    overlayStyle={styles.overlay}
   >
     <SearchBar
-      placeholder='Type here...'
+      placeholder='Search recipe by title...'
       onChangeText={updateSearch}
       value={title}
-      containerStyle={{
-        backgroundColor: '#fff',
-        borderBottomColor: 'transparent',
-        borderTopColor: 'transparent'
-      }}
+      containerStyle={styles.searchBar}
       inputContainerStyle={{ backgroundColor: '#fff' }}
-      searchIcon={{ size: 20 }}
+      searchIcon={{ size: 0 }}
       showLoading
       showCancel
       lightTheme
       on
     />
-    <TouchableOpacity onPress={() => onSubmitSearch()}>
-      <Text>Etsi</Text>
-    </TouchableOpacity>
+    <Button
+      onPress={() => onSubmitSearch()}
+      style={styles.searchButton}
+    >
+      <Icon name='search1' size={20} />
+    </Button>
   </Overlay>
 )
 
